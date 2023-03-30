9705c5b16b5204a5dcbf3cd28c5f9d8a6d321a07
rishilokeh <113780914+rishilokeh@users.noreply.github.com>
2023-03-30 12:27:24 +0530

    Update duplicate_values.java

--- duplicate_values.java
+++ duplicate_values.java
@@ -1,30 +1,16 @@
-// simple java program to remove duplicates
-
 class duplicate_values {
-	// Function to remove duplicate elements This function
-	// returns new size of modified array.
 	static int removeDuplicates(int arr[], int n)
 	{
-		// Return, if array is empty or contains a single
-		// element
 		if (n == 0 || n == 1)
 			return n;
 
 		int[] temp = new int[n];
-
-		// Start traversing elements
 		int j = 0;
 		for (int i = 0; i < n - 1; i++)
-			// If current element is not equal to next
-			// element then store that current element
 			if (arr[i] != arr[i + 1])
 				temp[j++] = arr[i];
 
-		// Store the last element as whether it is unique or
-		// repeated, it hasn't stored previously
 		temp[j++] = arr[n - 1];
-
-		// Modify original array
 		for (int i = 0; i < j; i++)
 			arr[i] = temp[i];
 
@@ -37,11 +23,7 @@ class duplicate_values {
 		int n = arr.length;
 
 		n = removeDuplicates(arr, n);
-
-		// Print updated array
 		for (int i = 0; i < n; i++)
 			System.out.print(arr[i] + " ");
 	}
 }
-
-// This code is contributed by Aditya Kumar (adityakumar129)
