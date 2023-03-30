7a82f10927307d2563589fb8f42b668de23f3769
rishilokeh <113780914+rishilokeh@users.noreply.github.com>
2023-03-30 12:32:27 +0530

    Update project_1.java

--- project_1.java
+++ project_1.java
@@ -1,30 +1,18 @@
-// simple java program to remove duplicates
-
 class duolicate values {
-	// Function to remove duplicate elements This function
-	// returns new size of modified array.
 	static int removeDuplicates(int arr[], int n)
 	{
-		// Return, if array is empty or contains a single
-		// element
 		if (n == 0 || n == 1)
 			return n;
 
 		int[] temp = new int[n];
 
-		// Start traversing elements
 		int j = 0;
 		for (int i = 0; i < n - 1; i++)
-			// If current element is not equal to next
-			// element then store that current element
 			if (arr[i] != arr[i + 1])
 				temp[j++] = arr[i];
 
-		// Store the last element as whether it is unique or
-		// repeated, it hasn't stored previously
 		temp[j++] = arr[n - 1];
 
-		// Modify original array
 		for (int i = 0; i < j; i++)
 			arr[i] = temp[i];
 
@@ -38,10 +26,7 @@ class duolicate values {
 
 		n = removeDuplicates(arr, n);
 
-		// Print updated array
 		for (int i = 0; i < n; i++)
 			System.out.print(arr[i] + " ");
 	}
 }
-
-// This code is contributed by Aditya Kumar (adityakumar129)
